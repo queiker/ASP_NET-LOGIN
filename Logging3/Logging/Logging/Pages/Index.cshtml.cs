6157c0dfdb3d4e13eb8981090bc57db8f7b447da
Grzegorz Płonka Kraków ul.Źródlana 11 30-734 <tradecomp.pl@gmail.com>
2021-07-03 16:03:32 +0200

    dodane dwa rodzaje logowania w asp net

---
+++ Logging3/Logging/Logging/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Logging.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public string Message { get; set; }
+        public string PageTitle => _configuration["PageTitle"];
+
+        public void OnGet()
+        {
+            Message = $"About page visited at {DateTime.UtcNow.ToLongTimeString()}";
+            _logger.LogInformation(Message);
+            _logger.LogCritical("this is critical");
+      ;
+        }
+    }
+}
